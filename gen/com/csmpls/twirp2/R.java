0cdb249af17c7101f4a21d2758f92e898bf35c22
nick merrill <nick.j.merrill@gmail.com>
2012-12-25 22:36:42 -0800

    started on timepickerdialog

--- gen/com/csmpls/twirp2/R.java
+++ gen/com/csmpls/twirp2/R.java
@@ -38,6 +38,7 @@ public final class R {
     public static final class menu {
         public static final int activity_alarm_set=0x7f080000;
         public static final int activity_main=0x7f080001;
+        public static final int activity_tweet=0x7f080002;
     }
     public static final class raw {
         public static final int alarm_file=0x7f040000;
